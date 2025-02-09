2234685e8000b079279ee2454b44606902f0d58d
Lukas Dev <103273579+Lukasrangel@users.noreply.github.com>
2025-02-09 20:27:32 -0300

    Create rifa_punk_e_amizade.py

---
+++ rifa_punk_e_amizade.py
@@ -0,0 +1,18 @@
+import random
+
+# software para o sorteio da rifa Punk é amizade!
+
+
+print()
+print("Sorteio da rifa Punk é Amizade!");
+print()
+print("####################################");
+print()
+print()
+
+premio = input("Prêmio: ");
+
+
+num = random.randint(1,200)
+print("Número sorteado:")
+print(num)
